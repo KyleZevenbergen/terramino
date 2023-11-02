f6dfb7f9baba8790731faa5dac560592034eeb0e
kylezevenbergen <zevenbergen.kyle@gmail.com>
2023-11-02 12:42:26 -0500

    checking sentinel policy

--- variables.tf
+++ variables.tf
@@ -26,7 +26,7 @@ variable "subnet_prefix" {
 
 variable "instance_type" {
   description = "Specifies the AWS instance type."
-  default     = "t3.large"
+  default     = "t3.2xlarge"
 }
 
 variable "admin_username" {

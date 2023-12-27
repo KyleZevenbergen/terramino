6e97c1ed355630f957cbf8f555d4a283d0706ba5
KyleZevenbergen <zevenbergen.kyle@gmail.com>
2023-12-27 14:00:03 -0600

    testing run

--- variables.tf
+++ variables.tf
@@ -26,7 +26,7 @@ variable "subnet_prefix" {
 
 variable "instance_type" {
   description = "Specifies the AWS instance type."
-  default     = "t3.2xlarge"
+  default     = "t3.micro"
 }
 
 variable "admin_username" {
